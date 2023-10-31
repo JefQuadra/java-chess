4ca3c02143fc404e5e669754ef444bf1111f62c5
Mariana Almeida de Azevedo <jefquadra@gmail.com>
2023-10-31 19:15:19 -0300

    Create table.

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import chess.ChessMatch;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        
+        ChessMatch chessMatch = new ChessMatch();
+
+        UI.printBoard(chessMatch.getPieces());
+
+    }
+}
