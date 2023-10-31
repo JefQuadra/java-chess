06b0251e41b3c7a4f249f00e37ca9fbaab38b167
Mariana Almeida de Azevedo <jefquadra@gmail.com>
2023-10-31 18:32:21 -0300

    Create first class.

---
+++ src/application/App.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
