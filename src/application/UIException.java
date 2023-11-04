320ef8b4019c421e3424cd670cc56121a8893b19
Mariana Almeida de Azevedo <jefquadra@gmail.com>
2023-11-04 17:55:30 -0300

    Handling exceptions and clearing screen.

---
+++ src/application/UIException.java
@@ -0,0 +1,8 @@
+package application;
+
+public class UIException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+    public UIException (String msg) {
+        super(msg);
+    }
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess_pieces.King;
 import chess_pieces.Rook;
 
@@ -21,6 +23,27 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+        validateSourcePosition(source);
+        Piece capturePiece = makeMove(source, target);
+        return (ChessPiece) capturePiece;
+    }
+
+    private Piece makeMove(Position source, Position target) {
+        Piece p = board.removePiece(source);
+        Piece capturePiece = board.removePiece(target);
+        board.placePiece(p, target);
+        return capturePiece;
+    }
+
+    private void validateSourcePosition(Position source) {
+        if (!board.thereIsAPiece(source)) {
+            throw new ChessException("There is no piece on source position.");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
